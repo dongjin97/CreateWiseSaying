2d065248b5adf359507ff6b8f342cf1a57098c9c
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-03-16 20:36:35 +0900

    addGitrepo

--- CreateWiseSaying/CreateWiseSaying/ViewController.swift
+++ CreateWiseSaying/CreateWiseSaying/ViewController.swift
@@ -3,22 +3,15 @@
 //  CreateWiseSaying
 //
 //  Created by 원동진 on 2022/03/16.
-//
+//start
+//start
+import UIKit
 
-import Cocoa
-
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
